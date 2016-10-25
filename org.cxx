0a3ca25dfdaf7303e2c2530a2035981850df53c4
Basilis <user@MacBook-Pro-user.local>
2016-10-26 02:59:17 +0300

    Tomita-hw

--- org.cxx
+++ org.cxx
@@ -1,5 +1,33 @@
 #encoding "utf-8"
 
-OrgDescr -> AnyWord<kwtype="org_descr">;
+OrgDescr -> Word<kwtype="org_descr">;
+OrgDescr -> Adj<gnc-agr[1]>+  OrgDescr<gnc-agr[1]>;
 
-Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
+QuotedW -> Word<quoted, h-reg1>;
+QuotedW -> Word<h-reg1, l-quoted> Word<~r-quoted>* AnyWord<r-quoted>;
+
+//Org -> Word<wff=/[А-ЯA-Z]{2,10}/, kwtype=~"country_short_name"> interp(Object.Name; Object.Type="ORG");
+OrgName -> UnknownPOS<wff=/[а-яА-Я]+/>;
+OrgName -> Word<h-reg1, GU=["gen"]> Word<GU=["gen"]>*;
+OrgName -> Word<h-reg1, GU=["nom"]>;
+OrgName -> Word<h-reg1, lat>+;
+OrgName -> QuotedW;
+OrgName -> 'по' Word<gram="dat">+;
+
+Org -> AnyWord<wff=/.*\.((ru)|(ру)|(com)|(org))/> interp(Object.Name; Object.Type="ORG");
+
+
+// change
+//Org -> (Adj<h-reg1, gnc-agr[1]> interp(Object.Name; Object.Type="ORG")) OrgDescr<gnc-agr[1]> interp(Object.Descr) (OrgName interp(Object.Name; Object.Type="ORG"));
+
+//Org -> Adj<h-reg1, gnc-agr[1]>+ OrgDescr<gnc-agr[1]> interp(Object.Descr) OrgName interp(Object.Name; Object.Type="ORG");
+Org -> Word<kwtype="organisation"> interp(Object.Name; Object.Type="ORG");
+Org -> Adj<h-reg1, gnc-agr[1]>+ interp(Object.Name; Object.Type="ORG") OrgDescr<gnc-agr[1]> interp(Object.Descr);
+Org -> OrgDescr interp(Object.Descr) OrgName interp(Object.Name; Object.Type="ORG");
+
+
+//Org -> OrgDescr interp(Object.Descr) UnknownPOS<wff=/[а-яА-Я]+/> interp(Object.Name; Object.Type="ORG");
+//Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1, GU=["gen"]|["nom"]> interp(Object.Name; Object.Type="ORG");
+//Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1, lat>+ interp(Object.Name; Object.Type="ORG");
+//Org -> OrgDescr interp(Object.Descr) QuotedW interp(Object.Name; Object.Type="ORG");
+//Org -> Adj<h-reg1, gnc-agr[1]> interp(Object.Name; Object.Type="ORG") OrgDescr<gnc-agr[1]> interp(Object.Descr);
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir reports
 for f in ../factRuEval-2016/devset/*.txt; 
 do 
   name=`echo $f | grep -E -o "book_[0-9]+"`;
-  cat $f | ../tomita-parser/build/bin/tomita-parser config.proto > facts/$name.proto ;
+  cat $f | tomita-mac config.proto > facts/$name.proto ;
 done
 
 for f in facts/*.proto; 
